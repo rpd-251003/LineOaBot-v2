d9aebce679937190fe9df1cbd73fe0642cb411df
TxGabz <44201252+TxGabz@users.noreply.github.com>
2018-11-03 19:20:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -305,7 +305,7 @@ function manga_syn($title) {
     return $result;
 }
 function musiknya($keyword) {
-    $uri = "https://rest.farzain.com/api/joox.php?apikey=fDh6y7ZwXJ24eiArhGEJ55HgA&id=" . $keyword;
+    $uri = "https://rest.farzain.com/api/joox.php?id=" .$keyword. "&apikey=fDh6y7ZwXJ24eiArhGEJ55HgA";
     $response = Unirest\Request::get("$uri");
     $json = json_decode($response->raw_body, true);
     $result = "「Music Result」\n";
